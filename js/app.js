1476b5d055467175859571215e53fad70339f713
Saqib Aziz <83906732+Saqib-Aziz33@users.noreply.github.com>
2021-09-26 23:43:31 -0700

    2.0.0

--- js/app.js
+++ js/app.js
@@ -1,28 +1,34 @@
-/* typed */
+/* dark theme */
+const themeBtn = document.getElementById(`theme-btn`)
+const darkTheme = 'dark-theme'
+const selectedTheme = localStorage.getItem('selected-theme')
+const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light'
+if (selectedTheme) {
+    document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
+}
+themeBtn.addEventListener(`click`, () => {
+    document.body.classList.toggle(darkTheme)
+    localStorage.setItem('selected-theme', getCurrentTheme())
+})
+
+
+
+/* typed.js */
 var typed = new Typed('.element', {
-    strings: 
-    [
-        '',
-        'am a Front End Web Developer.',
-        'love every thing about code.',
-        'am learning full stack Development.',
-        'am a Freelancer.',
-        'am a Developer'
-        
-    ],
-    smartBackspace: true, // Default value
-    loop: false,
-    //loopCount: Infinity,
-    typeSpeed: 30
+    strings: ['', 'am a front end web developer', 'am learning backend web development', 'am photoshop expert', 'am a Developer'],
+    smartBackspace: true,
+    typeSpeed: 25
 });
-
-
-/* animate on scroll */
-AOS.init();
 
 
 /* footer year */
 const yearDiv = document.getElementById(`year`);
 const date = new Date();
-
 yearDiv.innerText = date.getFullYear();
+
+
+/* animate on scroll */
+AOS.init({
+    offset: 250,
+    duration: 800
+});
