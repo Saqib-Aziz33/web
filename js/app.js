8f19864d4057cb46bd6bad2d3da388d19a1c647b
Saqib Aziz <83906732+Saqib-Aziz33@users.noreply.github.com>
2021-10-28 11:29:13 -0700

    v3.0

--- js/app.js
+++ js/app.js
@@ -1,34 +1,84 @@
-/* dark theme */
-const themeBtn = document.getElementById(`theme-btn`)
+//navbar logic
+const navToggler = document.querySelector('.nav-toggler')
+const showNav = document.querySelector('.my-nav-links')
+const portfolioNavigationBtn = document.querySelector('.portfolio-navigation-btn')
+
+navToggler.addEventListener('click', () => {
+    showNav.classList.toggle('active')
+    portfolioNavigationBtn.classList.toggle('in-active')
+})
+
+
+const navbar = document.querySelector('header')
+window.addEventListener(`scroll`, () => {
+    if(window.pageYOffset > 500){
+        navbar.classList.add(`active`);
+    }
+    else{
+        navbar.classList.remove(`active`);
+    }
+})
+
+
+
+//dark theme logic
+
+//theme btn name
+const btn = document.getElementById('dark-theme-toggler')
+const themeMsg = document.getElementById('theme-msg')
+//class name
 const darkTheme = 'dark-theme'
 const selectedTheme = localStorage.getItem('selected-theme')
 const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light'
 if (selectedTheme) {
     document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
 }
-themeBtn.addEventListener(`click`, () => {
+btn.addEventListener('click', () => {
     document.body.classList.toggle(darkTheme)
     localStorage.setItem('selected-theme', getCurrentTheme())
 })
 
 
 
-/* typed.js */
-var typed = new Typed('.element', {
-    strings: ['', 'am a front end web developer', 'am learning backend web development', 'am photoshop expert', 'am a Developer'],
-    smartBackspace: true,
-    typeSpeed: 25
-});
+//typed.js
+var options = {
+    strings: [
+        ``,
+        `I'm a Front end <span class="clr-primary">Web Developer</span>.`,
+        `I'm a Full stack <span class="clr-primary">Javascript Developer.</span>`,
+        `I'm a <span class="clr-primary">Photographer.</span>`
+    ],
+    typeSpeed: 100,
+    loop: true
+};
+  
+var typed = new Typed('.element', options);
 
 
-/* footer year */
-const yearDiv = document.getElementById(`year`);
-const date = new Date();
-yearDiv.innerText = date.getFullYear();
+
+//portfolio
+const frontend = document.getElementById('frontend')
+const backend = document.getElementById('backend')
+const front = document.getElementById('front')
+const back = document.getElementById('back')
+
+front.addEventListener('click', () => {
+    backend.style.display = 'none'
+    frontend.style.display = 'block'
+    front.classList.add('active')
+    back.classList.remove('active')
+})
+back.addEventListener('click', () => {
+    frontend.style.display = 'none'
+    backend.style.display = 'block'
+    front.classList.remove('active')
+    back.classList.add('active')
+})
 
 
-/* animate on scroll */
+
+
+//animate on scroll
 AOS.init({
-    offset: 250,
-    duration: 800
+    duration: 2000
 });
